a873a14dac3377a259e501b7cc102966044e2043
brecht.derwael@gmail.com <brecht.derwael@gmail.com@ef38b6cf-2a21-a8fb-6aac-88b5813d764d>
2012-01-19 03:19:47 +0000

    Wa n gesmodder eh

--- trunk/TestProject/TestProject/Form1.cs
+++ trunk/TestProject/TestProject/Form1.cs
@@ -6,6 +6,9 @@ using System.Drawing;
 using System.Linq;
 using System.Text;
 using System.Windows.Forms;
+using Microsoft.Research.DynamicDataDisplay.DataSources.MultiDimensional;
+using Microsoft.Research.DynamicDataDisplay.Common.Auxiliary;
+using Microsoft.Research.DynamicDataDisplay.Charts;
 
 namespace TestProject
 {
@@ -26,6 +29,35 @@ namespace TestProject
         {
             SqlToExcelForm form = new SqlToExcelForm();
             form.Show();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            int width = 50;
+            int height = 50;
+
+            double[,] data = new double[width, height];
+
+            // Hier moet ik de dingskes van de database krijge
+
+            Point[,] gridData = new Point[width, height];
+
+            for (int x = 0; x < width; x++)
+            {
+                for (int y = 0; y < height; y++)
+                {
+                    gridData[y, x] = new Point(y, x);   // Kan zijn da ge de x en y moet spiegele hier, ge ziet maar
+                }
+            }
+
+            // As ik dit hier in ne .xaml krijg denk ik da het wel gaat werke
+            WarpedDataSource2D<double> dataSource = new WarpedDataSource2D<double>(data, gridData);
+
+            isolineGraph.DataSource = dataSource;
+            trackingGraph.DataSource = dataSource;
+
+            Rect visible = dataSource.GetGridBounds();
+            plotter.Viewport.Visible = visible;
         }
     }
 }
