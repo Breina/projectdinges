503c4b4d2898b8a28a78ca53690534102085bc3d
brecht.derwael@gmail.com <brecht.derwael@gmail.com@ef38b6cf-2a21-a8fb-6aac-88b5813d764d>
2012-01-24 10:47:35 +0000

    OMNIFIX

---
+++ TestProject2/TestProject/ExcelToSqlForm.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using TestDB;
+using System.IO;
+using System.Data.SqlClient;
+
+namespace TestProject
+{
+    public partial class ExcelToSqlForm : Form
+    {
+        private string fileName;
+        private TestDB1 test;
+        public ExcelToSqlForm()
+        {
+            InitializeComponent();
+            zetOmButton.Enabled = false;
+        }
+
+        private void kiesExcelButton_Click(object sender, EventArgs e)
+        {
+            openFileDialog1.Filter = "Excelbestanden(*.xlsx)|*.xlsx";
+            if (openFileDialog1.ShowDialog() == DialogResult.OK)
+            {
+                fileName = openFileDialog1.FileName;
+                fileTextBox.Text = openFileDialog1.FileName;
+                zetOmButton.Enabled = true;
+            }
+        }
+
+        private void zetOmButton_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                test = new TestDB1();
+            test.excelToSql(openFileDialog1.FileName);
+              
+            }
+            catch (SqlException ex)
+            {
+                MessageBox.Show(ex.StackTrace, ex.Message);
+            }
+        }
+
+        private void cancelButton_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+    }
+}
